2bc6c632faf6c6138e0632887669050417dbc27d
craigbrett17 <craigbrett17@aol.com>
2014-07-27 21:26:25 +0100

    Added a batch file to make both installers for us

---
+++ Blindspot/Make installers.bat
@@ -0,0 +1,10 @@
+@ECHO OFF
+echo Making installers for both .NET and no .NET
+call "%programfiles%\NSIS\makensis.exe" /DINCLUDE_DOTNET=0 "Blindspot Installer.nsi" >nul
+call "%programfiles%\NSIS\makensis.exe" /DINCLUDE_DOTNET=1 "Blindspot Installer.nsi" >nul
+echo Finished making installers
+echo.
+
+echo gzipping up no .NET installer and naming to blindspot_dev.exe.gz
+call 7za a -tgzip "blindspot_dev.exe.gz" "Blindspot Installer no DotNet.exe" >nul
+pause
